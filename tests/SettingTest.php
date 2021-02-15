be45f9f5e2beef94a867305db9b1b5aa7f70ab76
Denis Pankratov <dpankratov@ronasit.com>
2021-02-15 01:15:57 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- tests/SettingTest.php
+++ tests/SettingTest.php
@@ -112,7 +112,7 @@ class SettingTest extends TestCase
     {
         $setting = $this->getJsonFixture('update_defaults_setting.json');
 
-        $response = $this->actingAs($this->admin)->json('put', "/settings/defaults", $setting);
+        $response = $this->actingAs($this->admin)->json('put', '/settings/defaults', $setting);
 
         $response->assertStatus(Response::HTTP_NO_CONTENT);
 
