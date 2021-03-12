275902c19f847024a6fb783237de857abf9cb023
Denis Pankratov <dpankratov@ronasit.com>
2021-03-12 11:14:35 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- app/Repositories/JobRepository.php
+++ app/Repositories/JobRepository.php
@@ -66,7 +66,7 @@ class JobRepository extends BaseRepository
                 }
                 if (Arr::has($this->filter, 'start_time_to')) {
                     $time = Carbon::createFromFormat('Y-m-d H:i:s', $this->filter['start_time_to'])->format('H:i');
-                    $query->where(DB::raw("cast(start_time as time)"), '<=', $time);
+                    $query->where(DB::raw('cast(start_time as time)'), '<=', $time);
                 }
                 if (Arr::has($this->filter, 'end_time_from')) {
                     $time = Carbon::createFromFormat('Y-m-d H:i:s', $this->filter['end_time_from'])->format('H:i');
